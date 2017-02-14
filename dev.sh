68848eb8d89aaeca4b0ac80e2649fd611fe97fad
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2017-02-14 14:58:17 +0100

    update dev-script

--- dev.sh
+++ dev.sh
@@ -1,5 +1,5 @@
 if [ ! -e node_modules/.bin/live-server ]; then npm install live-server eslint; fi
-cat direape.js | sed -e 's/^/    /' | sed -e 's/^ *[/][/] \?//' > README.md
+cat reun.js | sed -e 's/^/    /' | sed -e 's/^ *[/][/] \?//' > README.md
 ./node_modules/.bin/eslint *.js;
 
 ./node_modules/.bin/live-server --no-browser &
